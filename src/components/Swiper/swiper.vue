3407e70fffb41cfb7511684b14e6e80560bc4ea4
xin0710 <xin.wxh@qq.com>
2023-07-04 23:36:23 +0800

    基于vue2+springboot的图书管理系统

---
+++ src/components/Swiper/swiper.vue
@@ -0,0 +1,102 @@
+
+  <template>
+  <!--基础存放容器-->
+  <div class="swiper-container">
+    <!-- 需要进行轮播的部分 -->
+    <div class="swiper-wrapper">
+      <!-- 每个节点 -->
+      <div class="swiper-slide">
+        <img
+        data-src="https://cdn.staticaly.com/gh/xin0710/picture@master/img/book.png"
+          src="https://xxx.xiaobaitiao.club/img/loading.gif"
+          alt="书籍1"
+          class="swiper-lazy"
+        />
+      </div>
+      <div class="swiper-slide">
+        <img
+          data-src="https://cdn.staticaly.com/gh/xin0710/picture@master/img/book4.png"
+          src=""
+          alt="书籍2"
+          class="swiper-lazy"
+        />
+        <div class="swiper-lazy-preloader"></div>
+      </div>
+      <div class="swiper-slide">
+        <img
+          data-src="https://cdn.staticaly.com/gh/xin0710/picture@master/img/book.png"
+          src=""
+          alt="书籍3"
+          class="swiper-lazy"
+        />
+        <div class="swiper-lazy-preloader"></div>
+      </div>
+    </div>
+
+    <!--如果需要使用分页器-->
+    <div class="swiper-pagination swiper-pagination-white"></div>
+
+    <!-- 如果需要使用前进后退按钮 -->
+    <div class="swiper-button-prev swiper-button-white"></div>
+    <div class="swiper-button-next swiper-button-white"></div>
+  </div>
+</template>
+
+
+
+<script>
+import swiper from "swiper";
+
+export default {
+  name: "",
+  data() {
+    return {};
+  },
+  mounted() {
+    this.initSwiper();
+  },
+  methods: {
+    initSwiper() {
+      new Swiper(".swiper-container", {
+        //设置轮播的循环方式
+        loop: true,
+
+        //设置自动播放间隔时间
+        autoplay: 2000,
+
+        // 轮播效果,默认为平滑轮播
+        effect: "slide",
+
+        //分页器
+        pagination: ".swiper-pagination",
+
+        //前进后退按钮
+        prevButton: ".swiper-button-prev",
+        nextButton: ".swiper-button-next",
+
+        // 用户中断轮播后续播
+        autoplayDisableOnInteraction: false,
+        //懒加载
+        lazyLoading: true,
+        
+
+      });
+    },
+  },
+};
+</script>
+
+
+<style scoped>
+@import "../../../node_modules/swiper/dist/css/swiper.css";
+.swiper-container {
+  width: 1000px;
+  height: 650px;
+  margin-top: 40px;
+}
+
+.swiper-slide img {
+  width: 100%;
+  object-fit: cover;
+}
+</style>
